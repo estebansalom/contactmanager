267b4858745b98f7da4deba91a59329e953198ab
estebansalom <esteban.salom@gmail.com>
2018-11-04 22:15:12 -0600

    Updates

---
+++ precache-manifest.2269ecee4fc17da3ccdc3d5e5b365966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "1dc219675d512970ed83",
+    "url": "/contactmanager/static/js/main.1dc21967.chunk.js"
+  },
+  {
+    "revision": "ef16b0d82ba414ffa87f",
+    "url": "/contactmanager/static/js/1.ef16b0d8.chunk.js"
+  },
+  {
+    "revision": "1dc219675d512970ed83",
+    "url": "/contactmanager/static/css/main.b215b647.chunk.css"
+  },
+  {
+    "revision": "ef16b0d82ba414ffa87f",
+    "url": "/contactmanager/static/css/1.db379757.chunk.css"
+  },
+  {
+    "revision": "05c7df3b33638777061b3bf72298d59c",
+    "url": "/contactmanager/index.html"
+  }
+];
