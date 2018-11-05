d4c83a6cf3cd2c5bc0582b27319272c8d11389fb
estebansalom <esteban.salom@gmail.com>
2018-11-04 20:44:26 -0600

    initial commit
    Initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  //componentWillMount() {
+  //console.log("componentWillMount");
+  //}
+
+  //componentDidUpdate() {
+  //console.log("componentDidUpdate");
+  //}
+  //componentWillUpdate() {
+  //console.log("componentWillMount");
+  //}
+  //componentWillRecieveProps(nextProps, nextState) {
+  //console.log("componentWillMount");
+  //}
+
+  //static getDerivedStateFromProps(nextProps, prevState) {
+  //  return null;
+  //}
+
+  //getSnapshotBeforeUpdate(prevProps, prevState){
+
+  //}
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+export default Test;
